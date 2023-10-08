e4020a2db9f3d5b12d95f5223954d2d82c847abc
Rohan Vaidya <rohanv.rvaidya@gmail.com>
2023-10-08 21:48:38 +0530

    test6

--- server.js
+++ server.js
@@ -1,9 +1,25 @@
 const express = require("express");
 const app = express();
+require("dotenv").config();
 const port = process.env.PORT || 80;
 
-require("dotenv").config();
+const mongoose = require("mongoose");
 
+const DBpass = process.env.DBpass;
+
+const dataSchema = new mongoose.Schema({
+  fullName: String,
+  branch: String,
+  email: String,
+  prn: String,
+  rollno: String,
+});
+
+const data = mongoose.model("data", dataSchema);
+
+mongoose.connect(
+  `mongodb+srv://edcviit:${DBpass}@cluster0.koohght.mongodb.net/FirstYearOrientation`
+);
 app.use(express.json());
 
 app.get("/", (req, res) => {
@@ -14,6 +30,14 @@ app.post("/submit", async (req, res) => {
   const { name } = req.body;
 
   console.log(name);
+  const newData = new data({
+    fullname: name,
+    branch: "demo",
+    email: "demo",
+    prn: "demo",
+    rollno: "demo",
+  });
+  await newData.save();
 
   res.status(200).send("Registration successful!");
 });
