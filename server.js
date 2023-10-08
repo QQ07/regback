862bdfc1f075a7634fd1ae73dff468d516e66f6a
Rohan Vaidya <rohanv.rvaidya@gmail.com>
2023-10-08 21:41:25 +0530

    test5

--- server.js
+++ server.js
@@ -4,8 +4,18 @@ const port = process.env.PORT || 80;
 
 require("dotenv").config();
 
+app.use(express.json());
+
 app.get("/", (req, res) => {
   res.send("Hello World!");
+});
+
+app.post("/submit", async (req, res) => {
+  const { name } = req.body;
+
+  console.log(name);
+
+  res.status(200).send("Registration successful!");
 });
 
 app.listen(port, () => {
